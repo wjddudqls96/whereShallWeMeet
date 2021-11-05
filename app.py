9c071fc6a82ba68ecaef1d426298cb1326a290e4
jybin96 <jybin96@changwon.ac.kr>
2021-11-05 13:37:40 +0900

    예외처리

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 from pymongo import MongoClient
 
 # client = MongoClient('localhost', 27017)
-client = MongoClient('mongodb://test:test@localhost', 27017)
+client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
 
