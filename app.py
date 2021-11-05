0b68b814807cc6a3ef8bdb5d71477b4b4fa504f4
jybin96 <jybin96@changwon.ac.kr>
2021-11-05 17:23:19 +0900

    최종본

--- app.py
+++ app.py
@@ -3,22 +3,57 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 from pymongo import MongoClient
+from flask_cors import CORS
 
+CORS(app)
 # client = MongoClient('localhost', 27017)
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
 
 ## HTML을 주는 부분
-@app.route('/')
-def home():
-    return render_template('./loginPage/yb_login.html')
+@app.route('/posting')
+def posting():
+    return render_template('/page/posting_page.html')
 
 
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
-    return render_template('/loginPage/login.html', msg=msg)
+    return render_template('/page/login_page.html', msg=msg)
+
+@app.route('/lovesort')
+def lovesort():
+    card_list = list(db.post.find({}).sort("love", -1))
+    print(card_list)
+    print('POST방식')
+    for card in card_list:
+        card['_id'] = str(card['_id'])  ### html에서는 objectid형을 못읽기 때문에 str로 변환해서 보내준다
+    return render_template('./page/main_page.html', card_list=card_list, sortType='좋아요순',title ='우리 어디서 만날까?')
+
+@app.route('/')
+def index():
+    #print('GET방식')
+    card_list = list(db.post.find({}))
+    print(card_list)
+    card_list.reverse()
+    print(card_list)
+    for card in card_list:
+        card['_id'] = str(card['_id']) ### html에서는 objectid형을 못읽기 때문에 str로 변환해서 보내준다
+    return render_template('./page/main_page.html', card_list=card_list, sortType='최신순', title ='우리 어디서 만날까?', cardType='all')
+
+@app.route('/mypage/<userid>')
+def mypage(userid):
+    print(userid)
+    print('GET방식')
+    card_list = list(db.post.find({'userId': userid}))
+    print(card_list)
+    card_list.reverse()
+    print(card_list)
+    for card in card_list:
+        card['_id'] = str(card['_id']) ### html에서는 objectid형을 못읽기 때문에 str로 변환해서 보내준다
+    return render_template('./page/main_page.html', card_list=card_list, sortType='최신순', title ='나만의 장소', cardType ='my')
+
 
 
 if __name__ == '__main__':
--- posting.py
+++ posting.py
@@ -7,50 +7,16 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
+from datetime import datetime
+from flask_cors import CORS
+
 #client = MongoClient('localhost', 27017)
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
-## HTML을 주는 부분
-@app.route('/lovesort')
-def lovesort():
-    card_list = list(db.post.find({}).sort("love", -1))
-    print(card_list)
-    print('POST방식')
-    for card in card_list:
-        card['_id'] = str(card['_id'])  ### html에서는 objectid형을 못읽기 때문에 str로 변환해서 보내준다
-    return render_template('./loginPage/yb/postDetailTest.html', card_list=card_list, sortType='좋아요순')
 
-@app.route('/')
-def index():
-    #print('GET방식')
-    card_list = list(db.post.find({}))
-    print(card_list)
-    card_list.reverse()
-    print(card_list)
-    for card in card_list:
-        card['_id'] = str(card['_id']) ### html에서는 objectid형을 못읽기 때문에 str로 변환해서 보내준다
-    return render_template('./loginPage/yb/postDetailTest.html',card_list=card_list,sortType='최신순',title = '우리 어디서 만날까?',cardType='all')
-
-@app.route('/mypage/<userid>')
-def mypage(userid):
-    print(userid)
-    print('GET방식')
-    card_list = list(db.post.find({'userId': userid}))
-    print(card_list)
-    card_list.reverse()
-    print(card_list)
-    for card in card_list:
-        card['_id'] = str(card['_id']) ### html에서는 objectid형을 못읽기 때문에 str로 변환해서 보내준다
-    return render_template('./loginPage/yb/postDetailTest.html',card_list=card_list,sortType='최신순',title = '나만의 장소',cardType = 'my')
-
-
-# @app.route('/postDetailPage/<data>')
-# def postDetailShow(data):
-#     print(json.loads(data))
-#     jsonChangeData = json.loads(data)   ##url에서 받아온 data는 str형태기 때문에 json형변환을 해준다음 넣어준다
-#
-#     return render_template('./loginPage/yb/postDetailPage.html', postDetailData= jsonChangeData)
+CORS(app)
 
 @app.route('/api/love',methods=['POST'])
 def love():
@@ -81,5 +47,49 @@ def deletePost():
     return jsonify({'success': True})
 
 
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+    address_receive = request.form['address_give']
+    location_receive = request.form['location_give']
+    nickname_receive = request.form['nickname_give']
+    userid_receive = request.form['userid_give']
+    file = request.files["file_give"]
+
+    location_receive = location_receive.split(',')
+    location_receive[0] = float(location_receive[0])
+    location_receive[1] = float(location_receive[1])
+    print(location_receive)
+    print(type(location_receive[0]))
+    extension = file.filename.split('.')[-1]
+
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+
+    filename = f'file-{mytime}'
+
+    save_to = f'static/{filename}.{extension}'
+    file.save(save_to)
+
+    doc = {
+        'title': title_receive,
+        'content': content_receive,
+        'address': address_receive,
+        'location': location_receive,
+        'imgFile': f'{filename}.{extension}',
+        'time': today.strftime('%Y.%m.%d'),
+        'userId': userid_receive,
+        'nickName': nickname_receive,
+        'loveClickUsers': [],
+        'love': 0
+
+    }
+
+    db.post.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
+
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=4000, debug=True)
